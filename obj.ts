587a55a898b2907efc1a2a3bec7d5adf5ba545a0
Uzma-Azam21 <uzmaazam241@gmail.com>
2024-03-25 11:53:18 -0700

    Add files

---
+++ obj.ts
@@ -0,0 +1,92 @@
+// Student Name : Uzma Azam
+// Registration no : 00330608
+// Day : Tuesday (9:am)
+// Class Assignment _ 3
+// Obects, Nested Obects and Arrays of OBjects
+
+// Task 1 _ Objects
+
+let regularStudent = {
+Name : "Miral",
+Age : 18,
+Grade : "A",
+Sport : "Football",
+Honors : true,
+}
+console.log(regularStudent);
+
+// Conditional Statement "if, else, if-else";
+let temperature = 25;
+let isRaining = true;
+
+if (temperature > 30){
+  if (isRaining) {
+    console.log("It's hot and raining outside!");
+  } else{
+    console.log("It's hot outside!");
+  }
+}
+else if (temperature > 20){
+  if (isRaining){
+    console.log ("It's warm and raining outside.");
+  } else{
+    console.log("It's warm outside.");
+  }
+} else {
+  if (isRaining) {
+    console.log("It's cold and raining outside.");
+  } else {
+    console.log("It's cold outside.");
+  }
+}
+
+// Task 2 _ Nested Objects
+const book = {
+title:"The Forty Rules of Love",
+author: "Elif Shafak",
+genre:  "Historical Fiction",
+details: {
+
+  publisher: "Penguin Books",
+  yearPublished: "March 2009",
+  rating: {
+    goodBooks: 4.8,
+    amazon: 4.5,
+
+  }
+}
+
+};
+console.log(book);
+
+// Task 3 _ Arrays of Object
+let books = [
+  { title: "The Forty Rules of Love", author: "Elif Shafak", year: 2009 },
+  { title: "Ten Thousand Doors of January", author: "Alix E. Harrow", year: 2019},
+  { title: "Think and Rich Grow", author: "Napoleon Hill", year: 1937 }
+];
+console.log(books);
+
+let Countries =[
+ {name: "Pakistan", nationalLanguage: "Urdu", capital: "Islamabad"},
+ {name: "Bangladesh", nationalLanguage: "Bengali", capital: "Dhaka"},
+ {name: "Saudi Arabia", nationalLanguage: "Arabic", capital: "Riyadh"},
+]
+console.log(Countries);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
